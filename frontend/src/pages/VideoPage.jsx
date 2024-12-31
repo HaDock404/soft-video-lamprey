7855d6244812d7d7d93ec31a08d6e563eaeb34cc
HaDock404 <delescluse.g@gmail.com>
2024-12-31 09:42:03 +0100

    homepage link

--- frontend/src/pages/VideoPage.jsx
+++ frontend/src/pages/VideoPage.jsx
@@ -1,8 +1,14 @@
 import '../styles/videopage.css'
+import HouseIcon from '../components/HouseIcon.jsx'
+import { Link } from 'react-router-dom'
 
 function VideoPage() {
     return (
         <section className="videopage">
+            <Link to='/' className="videopage_button_home">
+                <HouseIcon />
+                Home
+            </Link>
             Welcome in video Page
         </section>
     )
